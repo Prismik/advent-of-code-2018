9584ea8c3896179eafac732fe7466318020dcd0a
Francis Beauchamp <francis@lyrebird.ai>
2018-12-12 15:01:12 -0500

    Initial Commit

---
+++ Advent2018/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Advent2018
+//
+//  Created by Francis Beauchamp on 2018-12-12.
+//  Copyright © 2018 Francis Beauchamp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
